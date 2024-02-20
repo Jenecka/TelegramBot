b4bdf95e6c818d581db0fd7ec29981d473cb7db4
Maks Koles <mkolesinskiy@internet.ru>
2024-02-21 00:00:30 +0300

    Женька, кнопки работают!!!!!!!!!!

--- TgBot.py
+++ TgBot.py
@@ -1,1 +1,16 @@
-print("Леха крутой")
+import telebot
+from telebot import types
+
+token = "6828137414:AAGrFOHrYPeeIZ48oqOiE-xNdNxJVdS9sKY"
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button1 = types.KeyboardButton("Оформить литературу")
+    button2 = types.KeyboardButton("Проверить орфографию")
+    markup.add(button1, button2)
+    bot.send_message(message.from_user.id, "Salam", reply_markup=markup)
+# @bot.message_handler(commands=['button'])
+# def button_message(message):
+bot.infinity_polling()
