7109b8d0bddc5688457630f328bbb563b85447f0
евгений <eritvinskij@gmail.com>
2024-02-21 00:07:02 +0300

    feat: бот с ошибкой, Макс, помоги

--- TgBot.py
+++ TgBot.py
@@ -20,3 +20,21 @@ async def main():
     await dispatcher.start_polling()
 
 asyncio.run(main())
+
+
+# import telebot
+# from telebot import types
+#
+# token = "6828137414:AAGrFOHrYPeeIZ48oqOiE-xNdNxJVdS9sKY"
+# bot = telebot.TeleBot(token)
+#
+# @bot.message_handler(commands=['start'])
+# def start_message(message):
+#     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+#     button1 = types.KeyboardButton("Оформить литературу")
+#     button2 = types.KeyboardButton("Проверить орфографию")
+#     markup.add(button1, button2)
+#     bot.send_message(message.from_user.id, "Salam", reply_markup=markup)
+# # @bot.message_handler(commands=['button'])
+# # def button_message(message):
+# bot.infinity_polling()
