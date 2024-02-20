607f738294d2cb043362cb45ad24b17f1f4f8ca1
евгений <eritvinskij@gmail.com>
2024-02-20 20:54:36 +0300

    feat: бот с ошибкой, Макс, помоги

--- TgBot.py
+++ TgBot.py
@@ -1,5 +1,19 @@
-import telebot
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-bot = telebot.TeleBot("6828137414:AAGrFOHrYPeeIZ48oqOiE-xNdNxJVdS9sKY")
+bot = Bot(token="6828137414:AAGrFOHrYPeeIZ48oqOiE-xNdNxJVdS9sKY")
+dispatcher = Dispatcher(bot=bot)
 
-print("jsndfjsd")
+button1 = KeyboardButton(text="Оформить курсовую работу")
+button2 = KeyboardButton(text="Оформить список литературы")
+keyboard = ReplyKeyboardMarkup(keyboard=[[button1], [button2]], resize_keyboard=True)
+
+@dispatcher.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await message.answer("UR started, check keyboard", reply_markup=keyboard)
+
+async def main():
+    await dispatcher.start_polling()
+
+asyncio.run(main())
